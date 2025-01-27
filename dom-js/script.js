ced20d4cf1296538f35a31e07b44fd04c844691b
Zubeydali <160525888+Zubeydali@users.noreply.github.com>
2025-01-27 09:53:52 -0800

    new

--- dom-js/script.js
+++ dom-js/script.js
@@ -15,3 +15,56 @@ document.getElementById("applyColor").addEventListener("click", function() {
   const color = document.getElementById("colorInput").value;
   document.getElementById("text").style.color = color;
 });
+
+
+function calculate(operation) {
+  // İstifadəçi tərəfindən daxil edilən dəyərlər
+  const value1 = parseFloat(prompt("Enter first value:"));
+  const value2 = parseFloat(prompt("Enter second value:"));
+  
+  // Əgər daxil edilən dəyərlər keçərli deyilsə
+  if (isNaN(value1) || isNaN(value2)) {
+      alert('Please enter valid numbers!');
+      return;
+  }
+
+
+  let result;
+  switch (operation) {
+      case 'add':
+          result = value1 + value2;
+          break;
+      case 'subtract':
+          result = value1 - value2;
+          break;
+      case 'multiply':
+          result = value1 * value2;
+          break;
+      case 'divide':
+          if (value2 === 0) {
+              alert('Division by zero is not allowed!');
+              return;
+          }
+          result = value1 / value2;
+          break;
+      default:
+          alert('Invalid operation');
+          return;
+  }
+  
+
+  alert(`The result is: ${result}`);
+}
+
+
+console.log("Addition");
+calculate('add');
+
+console.log("Subtraction");
+calculate('subtract'); 
+
+console.log("Multiplication");
+calculate('multiply');
+
+console.log("Division");
+calculate('divide'); 
